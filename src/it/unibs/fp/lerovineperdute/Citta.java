816cfd4f1d5790fd532f1636a2d669b8deb14014
Zane2601 <d.zanelli002@studenti.unibs.it>
2019-05-17 16:28:54 +0200

    classe citta

---
+++ src/it/unibs/fp/lerovineperdute/Citta.java
@@ -0,0 +1,74 @@
+package it.unibs.fp.lerovineperdute;
+
+import java.util.ArrayList;
+
+public class Citta {
+	private int id;
+	private String nome;
+	private int x;
+	private int y;
+	private int z;
+	private ArrayList<Integer> collegamenti = new ArrayList<Integer>();
+	
+	
+	public Citta(int _id, String _nome, int _x, int _y, int _z) {
+		this.id=_id;
+		this.nome=_nome;
+		this.x=_x;
+		this.y=_y;
+		this.z=_z;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+
+	public int getZ() {
+		return z;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+	
+	//aggiungi nikita
+
+}
